70e6c648f4d98a4e6e49fb0bb23d713c10bad8a1
Richard Fox <richard.fox@ahref.co.uk>
2018-01-27 20:48:39 +0100

    0.9 support

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
 # prime-manual-alerts
 A manual alert sender. Listen to enable an alerts bundle to be tested by the user easily.
 
-This is a NodeCG 0.7 Bundle.
+This is a NodeCG 0.9 Bundle.
+
+![](img/demo.png)
+
+## Events
+
+### FOLLOW 
+
+``` 
+{
+	"username":"user",
+	"ts":<Unix timestamp>,
+	"type":"follow"
+}
+```
+
+### SUBSCRIPTION
+
+``` 
+{
+	"username":"user",
+	"ts":<Unix timestamp>,
+	"type":"subscription",
+	"months":2
+}
+```
+
+### HOST
+``` 
+{
+	"username":"user",
+	"ts":<Unix timestamp>,
+	"type":"host"
+}
+```
+
+### TIP
+
+``` 
+{
+	"username":"user",
+	"ts":<Unix timestamp>,
+	"type":"tip",
+	"amount":20,
+	"currencySymbol":"$"
+}
+```
+
+## Example Use
+
+```
+nodecg.listenFor('follow','prime-manual-alerts', data => {
+	console.log(data);
+});
--- dashboard/panel.js
+++ dashboard/panel.js
@@ -1,41 +1,37 @@
-'use strict';
-var $bundle = $('body');
-var $panel = $('body');
-var $type = document.getElementById('type');
-var $name = document.getElementById('name');
-var $amount = document.getElementById('tip');
-var $months = document.getElementById('months');
+const $type = document.getElementById('type');
+const $name = document.getElementById('name');
+const $amount = document.getElementById('tip');
+const $months = document.getElementById('months');
 
-var $send = document.getElementById('send');
+const $send = document.getElementById('send');
+
+function go() {
+	const username = $name.value;
+	const type = $type.selected;
+	const months = $months.value;
+
+	const extra = {};
+
+	if (type === 'subscription') {
+		extra.months = $months.value;
+	}
+
+	if (type === 'tip') {
+		// We default to Dollars to make the interface easier to manage,
+		// These are for test notifications after all
+		extra.amount = $amount.value;
+		extra.currencySymbol = '$';
+	}
+	sendEvent(type, username, extra);
+}
+
+function sendEvent(type, username, extra) {
+	nodecg.sendMessage(type, {
+		username,
+		type,
+		ts: Date.now(),
+		...extra
+	})
+}
 
 $send.addEventListener('click',go);
-function go() {
-    var name = $name.value;
-    var type = $type.selected;
-
-    var amount = $amount.value;
-
-    var months = $months.value;
-
-    if (type === 'subscription') {
-        nodecg.sendMessage('subscription', {
-            name: name,
-            ts:Date.now(),
-            months:months
-        });
-    } else if (type === 'follow') {
-        nodecg.sendMessage('follow', {
-            name: name,
-            ts:Date.now()
-        });
-    } else if (type === 'tip') {
-        //We default to Dollars to make the interface easier to manage,
-        //These are for test notifications afterall
-        nodecg.sendMessage('tip', {
-            name: name,
-            ts:Date.now(),
-            amount:amount,
-            currencySymbol:'$'
-        });
-    }
-}
